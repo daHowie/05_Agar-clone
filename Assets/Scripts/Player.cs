bf1a19068e4e6ae09f1fceb108abab9aab1cba86
dahowie <andreas.svarholt@gmail.com>
2022-02-26 22:20:50 +0100

    no message

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,37 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public LineRenderer circleRenderer;
-    public int steps = 100;
-    public float radius = 2f;
+    public float moveSpeed = 1f;
+    float horizontalAxis;
+    float verticalAxis;
 
-    void Start()
+    private void Update()
     {
+        horizontalAxis = Input.GetAxis("Horizontal");
+        verticalAxis = Input.GetAxis("Vertical");
     }
 
-    void Update()
+    private void FixedUpdate()
     {
-        DrawCircle(steps, radius);
-    }
-
-    void DrawCircle(int steps, float radius)
-    {
-        circleRenderer.positionCount = steps;
-
-        for (int currentStep = 0; currentStep < steps; currentStep++)
-        {
-            float circumferenceProgress = (float)currentStep / steps;
-            float currentRadian = circumferenceProgress * 2 * Mathf.PI;
-
-            float xScaled = Mathf.Cos(currentRadian);
-            float yScaled = Mathf.Sin(currentRadian);
-
-            float x = xScaled * radius;
-            float y = yScaled * radius;
-
-            Vector3 currentPosition = new Vector3(x, y, 0);
-
-            circleRenderer.SetPosition(currentStep, currentPosition);
-        }
+        float t = Time.fixedDeltaTime;
+        transform.Translate(horizontalAxis * t, verticalAxis * t, 0);
     }
 }
